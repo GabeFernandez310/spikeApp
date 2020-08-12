c8113ff85d56d7c0dce60311f0facd99cce8a777
Gabriel Fernandez <gabe.fer2000@gmail.com>
2020-08-12 11:39:16 -0700

    1st attempt: React

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 //Basic express app for running server
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
+const path = require("path");
 
+const app = express();
+
+//Connect to Database
 pool = connectDB();
 pool.connect();
 
-app.get("/", (req, res) => res.send("Hello world"));
+//TODO: Add api routes
+//app.get("/", (req, res) => res.send("Hello world"));
+
+//Will use static assests in a production environment
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
